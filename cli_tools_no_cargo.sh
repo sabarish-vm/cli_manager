0abe9be1f411248117641b70775a1bf6fc925a6a
sabarish-vm <sabarish.science@gmail.com>
2024-08-07 23:17:31 +0200

    cleanup + lazygit

--- cli_tools_no_cargo.sh
+++ cli_tools_no_cargo.sh
@@ -6,7 +6,7 @@ reldir="$HOME/opt/.temp-cli-release"
 mkdir -p "$bindir"
 
 ####### Install fzf
-echo '##### FZF ####'
+echo '##### fzf ####'
 if command -v fzf &>/dev/null; then
   echo 'Exists'
 else
@@ -41,9 +41,7 @@ else
 fi
 
 ####### Install rust command line tools
-# cargo install eza # TODO Can be changed to downloading release file rather than compiling from rust
 # cargo install --locked bat
-# cargo install ripgrep
 # cargo install tealdeer
 # cargo install du-dust
 # cargo install diskus
@@ -54,13 +52,12 @@ fi
 # eval "$(starship init zsh)"
 
 findlatestversion() {
-  tempname=$(
-    curl -Ls -o /dev/null -w "%{url_effective}" "$1"
-  )
+  tempname=$(curl -Ls -o /dev/null -w "%{url_effective}" "$1")
   echo "$tempname" | rev | cut -f 1 -d '/' | rev
 }
 
 getgitrelease() {
+
   mkdir -p "${reldir}"
   cd "${reldir}"
   mkdir contents
@@ -73,6 +70,7 @@ getgitrelease() {
   mv "$exec_path" "$bindir"
   rm "./contents" -rf
   rm -rf "${reldir}"
+
 }
 
 check_and_download() {
@@ -83,7 +81,21 @@ check_and_download() {
     exec_name="$1"
     download_link="$3"
     version=$(findlatestversion "$2")
-    printf "Latest version = $version"
+    download_link=$(echo "$download_link" | sed -E "s#VERSION#${version}#g")
+    getgitrelease "$download_link" "$exec_name"
+  fi
+
+}
+
+check_and_download_no_v() {
+
+  if command -v "$1"; then
+    :
+  else
+    exec_name="$1"
+    download_link="$3"
+    version=$(findlatestversion "$2")
+    version="${version/v/}"
     download_link=$(echo "$download_link" | sed -E "s#VERSION#${version}#g")
     getgitrelease "$download_link" "$exec_name"
   fi
@@ -99,10 +111,11 @@ just_download() {
     download_link="$2"
     getgitrelease "$download_link" "$exec_name"
   fi
+
 }
 
 printf '\n##### fd #####\n'
-check_and_download 'fd' 'https://github.com/sharkdp/fd/releases/latest/' 'https://github.com/sharkdp/fd/releases/download/VERSION/fd-VERSION-x86_64-unknown-linux-gnu.tar.gz'
+check_and_download 'fd' 'https://github.com/sharkdp/fd/releases/latest/' 'https://github.com/sharkdp/fd/releases/latest/download/fd-VERSION-x86_64-unknown-linux-gnu.tar.gz'
 
 printf '\n##### zellij #####\n'
 just_download 'zellij' 'https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz'
@@ -111,4 +124,7 @@ printf '\n##### eza #####\n'
 just_download 'eza' "https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz"
 
 printf '\n##### ripgrep #####\n'
-check_and_download 'rg' 'https://github.com/BurntSushi/ripgrep/releases/latest/' 'https://github.com/BurntSushi/ripgrep/releases/download/VERSION/ripgrep-VERSION-x86_64-unknown-linux-musl.tar.gz'
+check_and_download 'rg' 'https://github.com/BurntSushi/ripgrep/releases/latest/' 'https://github.com/BurntSushi/ripgrep/releases/latest/download/ripgrep-VERSION-x86_64-unknown-linux-musl.tar.gz'
+
+printf '\n##### lazygit #####\n'
+check_and_download_no_v 'lazygit' 'https://github.com/jesseduffield/lazygit/releases/latest/' 'https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_VERSION_Linux_x86_64.tar.gz'
