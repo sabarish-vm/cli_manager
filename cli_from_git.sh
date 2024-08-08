50e1e96f8a69832366b0543ba1910e131df99546
sabarish-vm <sabarish.science@gmail.com>
2024-08-08 08:38:33 +0200

    cleanup

--- cli_from_git.sh
+++ cli_from_git.sh
@@ -3,57 +3,11 @@
 bindir="$HOME/opt/bin/"
 reldir="$HOME/opt/.temp-cli-release"
 
-mkdir -p "$bindir"
-
-####### Install fzf
-echo '##### fzf ####'
-if command -v fzf &>/dev/null; then
-  echo 'Exists'
-else
-  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-  ~/.fzf/install
-fi
-
-####### Install zoxide
-echo '##### zoxide #####'
-if command -v zoxide &>/dev/null; then
-  :
-else
-  curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-  echo 'eval "$(zoxide init zsh)"' >>$HOME/.zshrc
-fi
-
-####### Install starship prompt
-
-echo '##### starship #####'
-if command -v starship &>/dev/null; then
-  :
-else
-  curl -sS https://starship.rs/install.sh | sh
-fi
-
-####### Install cargo and rust
-echo '##### rust #####'
-if command cargo 1>/dev/null 2>/dev/null; then
-  :
-else
-  curl https://sh.rustup.rs -sSf | sh
-fi
-
-####### Install rust command line tools
-# cargo install --locked bat
-# cargo install tealdeer
-# cargo install du-dust
-# cargo install diskus
-# cargo install git-delta
-# cargo install rnr
-# cargo install pipe-rename
-# Add this if needed
-# eval "$(starship init zsh)"
-
 findlatestversion() {
+
   tempname=$(curl -Ls -o /dev/null -w "%{url_effective}" "$1")
   echo "$tempname" | rev | cut -f 1 -d '/' | rev
+
 }
 
 getgitrelease() {
