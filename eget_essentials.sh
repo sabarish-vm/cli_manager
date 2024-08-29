ad5bc1ec5693b3d55f98d91b378ef1373825108c
sabarish-vm <sabarish.science@gmail.com>
2024-08-29 19:23:22 +0200

    more typos fixed

--- eget_essentials.sh
+++ eget_essentials.sh
@@ -2,10 +2,26 @@
 
 export EGET_BIN="${EGET_BIN:-$HOME/opt/bin/eget/}"
 export PATH="$EGET_BIN:$PATH"
+mkdir -p "${EGET_BIN}"
+echo "Placing the binaries in ${EGET_BIN}"
 
-filedir="$(realpath $(dirname $0))"
-source "${filedir}/eget_from_git.sh"
+if command -v "eget"; then
+    eget --upgrade-only zyedidia/eget
+else
+    curl -o eget.sh https://zyedidia.github.io/eget.sh
+    shasum -a 256 eget.sh # verify with hash below
+    bash eget.sh
+    rm eget.sh
+fi
 
+install_missing() {
+    if command -v "${1}"; then
+        :
+    else
+        eget --upgrade-only "$1"
+    fi
+
+}
 install_missing "junegunn/fzf"
 install_missing "ajeetdsouza/zoxide"
 install_missing "BurntSushi/ripgrep"
